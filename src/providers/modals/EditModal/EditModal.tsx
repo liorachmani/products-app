7ac8d6a55516465e6b8334e518f957ce82e74687
Lior Rachmani <liorachmani@gmail.com>
2024-01-18 12:06:07 +0200

    add editing row validation

--- src/providers/modals/EditModal/EditModal.tsx
+++ src/providers/modals/EditModal/EditModal.tsx
@@ -1,4 +1,9 @@
-import { MSWResponseBody, Product } from "@src/models";
+import {
+  MSWResponseBody,
+  Product,
+  currentAvailableBrands,
+  currentAvailableImages,
+} from "@src/models";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
 import { useModal } from "@src/providers";
@@ -11,21 +16,35 @@ import { Dialog } from "primereact/dialog";
 import { ProgressSpinner } from "primereact/progressspinner";
 import { Toast } from "primereact/toast";
 import { MutableRefObject, useState } from "react";
+import { ObjectSchema, object, string, number, ValidationError } from "yup";
 
 type EditModalProps = Product & {
   toast: MutableRefObject<Toast>;
+  open: boolean;
+  onClose: () => void;
 };
 type EditableFields = Product;
+
+const schema: ObjectSchema<Product> = object({
+  name: string()
+    .min(3, "Product name must be at least 3 characters")
+    .required(),
+  brand: string().oneOf(currentAvailableBrands).required(),
+  image: string().oneOf(currentAvailableImages).required(),
+  price: number().required(),
+  id: string().required(), // Maybe further check with http to ensure no duplicate id's
+});
 
 function EditModal(props: EditModalProps) {
   const [editProduct, { isLoading: isProductBeingUpdated }] =
     useEditProductMutation();
 
+  const [isValidRow, setIsValidRow] = useState<boolean>(true);
+
   const { closeModal } = useModal();
 
-  const { toast, ...canBeEdited } = props;
-
-  // const { id, image, ...canBeEdited } = props;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { toast, open, onClose, ...canBeEdited } = props;
 
   const [editedRowValues, setEditedRowValues] = useState<EditableFields>({
     ...canBeEdited,
@@ -36,16 +55,31 @@ function EditModal(props: EditModalProps) {
     rowHeight: 100,
     domLayout: "autoHeight",
     onCellEditingStopped: (event) => {
-      const colId = event.column.getColId() as keyof EditableFields;
+      try {
+        const colId = event.column.getColId() as keyof EditableFields;
 
-      //   console.log({ ...editedRowValues, [colId]: event.newValue });
+        const newRow = { ...editedRowValues, [colId]: event.newValue };
 
-      setEditedRowValues((prev) => ({ ...prev, [colId]: event.newValue }));
+        const parsedNewProduct = schema.validateSync(newRow);
+
+        setIsValidRow(true);
+        setEditedRowValues({ ...parsedNewProduct });
+      } catch (error) {
+        let errMsg = "An error occured ";
+        if (error instanceof ValidationError) errMsg += error.message;
+
+        toast.current.show({
+          severity: "error",
+          summary: "Error",
+          detail: errMsg,
+          life: 3000,
+        });
+        setIsValidRow(false);
+      }
     },
-    // onRowEditingStopped: (event) => console.log(event),
   };
 
-  const handleEditRow = async () => {
+  const handleEditRowSave = async () => {
     try {
       const payload: MSWResponseBody = await editProduct({
         ...editedRowValues,
@@ -81,8 +115,8 @@ function EditModal(props: EditModalProps) {
       <Button
         label="Save"
         severity="success"
-        onClick={handleEditRow}
-        disabled={isProductBeingUpdated}
+        onClick={handleEditRowSave}
+        disabled={isProductBeingUpdated || !isValidRow}
         text
         raised
       />
@@ -106,15 +140,18 @@ function EditModal(props: EditModalProps) {
       field: "brand",
       cellRenderer: undefined,
       editable: true,
+      cellEditor: "agSelectCellEditor",
+      cellEditorParams: {
+        values: currentAvailableBrands,
+      },
     },
     {
       field: "image",
-      // cellRenderer: undefined,
       cellRenderer: customCellRenderer,
       editable: true,
       cellEditor: "agSelectCellEditor",
       cellEditorParams: {
-        values: ["shirt", "pants", "socks", "shoes", "ball"],
+        values: currentAvailableImages,
       },
     },
     {
